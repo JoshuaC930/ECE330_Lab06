9c31350ba9ac0c60aef4485b0d23f44cb3d8cceb
Joshua Corrales <89368536+JoshuaC930@users.noreply.github.com>
2022-10-14 13:33:41 -0600

    Create SetBit.c

---
+++ SetBit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int setBit(int n, int position)
+{
+    if (n & (1 << position))
+    {
+        return 1;
+    }
+    else return 0;
+}
+
+int main() {
+    int i;
+    int position;
+    printf("Enter a number to check set bit and set bit count: ");
+    scanf("%d %d", &i, &position);
+    
+    printf("\n");
+    printf("\n ---------------------------------------------------------- \n");
+    //Write a program in “c” to find out a specific bit is set to 1 or 0.
+    printf("Bit set for number %d at position %d: %d \n", i, position, setBit(i, position));
+}
