9a78c35c1ee0c11d812c621b807de09f1c57414e
Joshua Corrales <89368536+JoshuaC930@users.noreply.github.com>
2022-10-17 00:14:15 -0600

    Update Swap.c

--- Swap.c
+++ Swap.c
@@ -1,38 +1,58 @@
-
-// Online C compiler to run C program online
 #include <stdio.h>
 
-void moveAllZeros(int Array[], int n)
+//Function to move all zeroes to left
+void moveAllZeroes(int arr[], int n)
 {
-    int index = 0;
-    int size = (int)sizeof(Array)/sizeof(Array[0]);
-    for (int i = size - 1; i >= 0; i--)
+    int count1 = 0;
+    
+    for (int i = 0; i < n; i++)
     {
-        if (Array[i] != 0)
+        if (arr[i] != 1)
         {
-            Array[size--] = Array[i];
+            arr[count1++] = arr[i];
+        }
+    }
+ 
+    while (count1 < n) 
+    {
+        arr[count1++] = 1;
+    }
+
+    int lastNonOne = 0;
+ 
+    // Traverse the array and pull non-zero elements to the
+    // required indices
+    for (int i = n - 1; i >= 0; i--) 
+    {
+        if (!lastNonOne)
+        {
+            lastNonOne = i;
+        }
+        if (arr[i] != 0)
+        {
+            arr[lastNonOne--] = arr[i];
         }
     }
     
-    for (int i = size; i < n; i++)
-    {
-        Array[i] = 0;
-    }
+    // Put zeros to start of array
+    while (lastNonOne >= 0)
+        arr[lastNonOne--] = 0;
 }
-int main() {
-    int array[] = {1,10,20,0,59,63,0,88,0}; 
+
+int main() 
+{
+    int array[] = {2,10,20,0,59,63,0,88,0}; 
     int arraySize = (int)sizeof(array)/sizeof(array[0]);
     
-    printf("sizeof of array: %d\n",(int)sizeof(array)/sizeof(array[0])); 
+    printf("sizeof of array: %d\n", arraySize); 
     
-    
-    moveAllZeros(array, arraySize);
-    
+    moveAllZeroes(array, arraySize);
+
     for (int i = 0; i < arraySize; i++)
     {
-        printf("%d, ", array[i]);
+        printf("%d ", array[i]);
     }
-    
+        
     
     return 0;
 }
